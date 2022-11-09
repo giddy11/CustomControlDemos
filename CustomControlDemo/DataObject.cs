0866a5a6585cc90298d8e94366af6c7b877cebff
gideon <edoghotugiddy@gmail.com>
2022-11-10 00:52:44 +0100

    Add project files.

---
+++ CustomControlDemo/DataObject.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace CustomControlDemo
+{
+    //public class DataObject : INotifyPropertyChanged
+    //{
+    //    private void NotifyPropertyChanged(string propertyName)
+    //    {
+    //        var handler = PropertyChanged;
+    //        if (handler != null)
+    //            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    //    }
+
+    //    private string _name;
+    //    public string Name
+    //    {
+    //        get { return _name; }
+    //        set
+    //        {
+    //            _name = value;
+    //            NotifyPropertyChanged("Name");
+    //        }
+    //    }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //}
+}
---
+++ WpfCustomControlLibrary1/MyTextControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControlLibrary1
+{
+    public class MyTextControl : Control
+    {
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MyTextControl), new FrameworkPropertyMetadata(0));
+
+
+        static MyTextControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyTextControl), new FrameworkPropertyMetadata(typeof(MyTextControl)));
+        }
+    }
+}
