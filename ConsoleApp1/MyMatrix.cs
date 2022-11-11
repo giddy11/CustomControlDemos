61baab20ee5c65220c249a486e45dc53bbb5b816
giddy11 <edoghotugiddy@gmail.com>
2022-11-11 16:57:53 +0100

    creating custom matrix class

---
+++ ConsoleApp1/MyMatrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public struct MyMatrix
+    {
+        public double Determinant { get; }
+        public bool HasInverse { get; }
+        public bool IsIdentity { get; }
+        public double M11 { get; set; }
+        public double M12 { get; set; }
+        public double M21 { get; set; }
+        public double M22 { get; set; }
+        public double OffsetX { get; set; }
+        public double OffsetY { get; set; }
+    }
+}
