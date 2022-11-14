9f0e2dc5538dbeb9b124f8de07cb71c52d47cf7b
gideon <edoghotugiddy@gmail.com>
2022-11-14 22:23:14 +0100

    updated

--- ConsoleApp1/MyMatrix.cs
+++ ConsoleApp1/MyMatrix.cs
@@ -14,27 +14,64 @@
         }
 
         //let the first matrix be denotd as a and the second matrix be denoted as b
-        public MyMatrix Multiply (MyMatrix a, MyMatrix b)
+        public static  MyMatrix Multiply (MyMatrix a, MyMatrix b)
         {   
             var ab11 = (a.M11 * b.M11) + (a.M12 * b.M21) + 0;
             var ab12 = (a.M11 * b.M12) + (a.M12 * b.M22) + 0;
 
             var ab21 = (a.M21 * b.M11) + (a.M22 * b.M21) + 0;
-            var ab22 = (a.M21 * b.M12) + (b.M22 * b.M22) + 0;
+            var ab22 = (a.M21 * b.M12) + (a.M22 * b.M22) + 0;
 
-            var abX = (a.OffsetX * b.M11) + (b.OffsetX * b.M21) + b.OffsetX;
-            var abY = (a.OffsetX * b.M12)+ (b.OffsetY * b.M22)+ b.OffsetY;
+            var abX = (a.OffsetX * b.M11) + (a.OffsetY * b.M21) + (1 * b.OffsetX) ;
+            var abY = (a.OffsetX * b.M12)+ (a.OffsetY * b.M22)+ (1 * b.OffsetY);
 
             var matrix = new MyMatrix(ab11, ab12, ab21, ab22, abX, abY);
             return matrix;
         }
 
-        public MyMatrix Scale (double scaleX, double scaleY)
+        public MyMatrix ScaleAt (MyMatrix matrix1, MyMatrix matrix2)
         {
-            var scaleVector = new MyMatrix(scaleX, 0, 0, scaleY, 0, 0);
-            var matrix = new MyMatrix(M11, M12, M21, M22, OffsetX, OffsetY);
-            return Multiply(matrix, scaleVector);
+            //return Multiply(matrix1, matrix2);
+            return matrix1 * matrix2;
         }
+
+        public MyMatrix Scale(double scaleX, double scaleY)
+        {
+            var scaleMatrix = new MyMatrix(scaleX, 0, 0, scaleY, 0, 0);
+            return scaleMatrix;
+        }
+
+        public MyMatrix Translate (double dx, double dy)
+        {
+            var translateMatrix = new MyMatrix(1, 0, 0, 1, dx, dy);
+            return translateMatrix;
+        }
+
+        public MyMatrix TranslateAt(MyMatrix matrix1, MyMatrix matrix2)
+        {
+            //return ScaleAt(matrix1, matrix2);
+            return matrix1 * matrix2;
+        }
+
+        public static MyMatrix operator * (MyMatrix matrix1, MyMatrix matrix2)
+        {
+
+            var multiply = Multiply(matrix1, matrix2);
+            return multiply;
+        }
+
+        public MyMatrix Rotate(double angle)
+        {
+            angle = angle * Math.PI / 180;
+            var rotateAngle = new MyMatrix(Math.Cos(angle), Math.Sin(angle), -Math.Sin(angle), Math.Cos(angle), 0, 0);
+            return rotateAngle;
+        }
+
+        public MyMatrix RotateAt(MyMatrix matrix1, MyMatrix matrix2)
+        {
+            return matrix1 * matrix2;
+        }
+
 
         //public double Determinant { get; }
         //public bool HasInverse { get; }
