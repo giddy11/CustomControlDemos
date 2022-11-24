19fecc4e1d33b610496537ea530469af61f74d53
giddy11 <edoghotugiddy@gmail.com>
2022-11-24 16:47:54 +0100

    office

---
+++ AnalogClockControl/CustomControls/AnalogClock.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AnalogClockControl.CustomControls
+{
+    public class AnalogClock : Control
+    {
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
+
+
+
+        public bool ShowSeconds
+        {
+            get { return (bool)GetValue(ShowSecondsProperty); }
+            set { SetValue(ShowSecondsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowSecondsProperty =
+            DependencyProperty.Register("ShowSeconds", typeof(bool), typeof(AnalogClock), new PropertyMetadata(true));
+
+
+        public override void OnApplyTemplate()
+        {
+            hourHand = (Line)Template.FindName("PART_HourHand", this);
+            minuteHand = (Line)Template.FindName("PART_MinuteHand", this);
+            secondHand = (Line)Template.FindName("PART_SecondHand", this);
+
+            //Binding showSecondsHandBinding = new Binding
+            //{
+            //    Path = new PropertyPath(nameof(ShowSeconds)),
+            //    Source = this,
+            //    Converter = new BooleanToVisibilityConverter()
+            //};
+
+            //secondHand.SetBinding(VisibilityProperty, showSecondsHandBinding);
+
+            UpdateHandAngle(DateTime.Now);
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += (s, e) => UpdateHandAngle(DateTime.Now);
+            timer.Start();
+
+            base.OnApplyTemplate();
+        }
+
+        private void UpdateHandAngle(DateTime time)
+        {
+            hourHand.RenderTransform = new RotateTransform((time.Hour / 12.0) * 360, 0.5, 0.5);
+            minuteHand.RenderTransform = new RotateTransform((time.Minute / 60.0) * 360, 0.5, 0.5);
+            secondHand.RenderTransform = new RotateTransform((time.Second / 60.0) * 360, 0.5, 0.5);
+        }
+
+
+        private Line hourHand;
+        private Line minuteHand;
+        private Line secondHand;
+    }
+}
