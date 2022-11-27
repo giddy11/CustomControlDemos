c25a97e633da46352e26e9febadae637790e632a
gideon <edoghotugiddy@gmail.com>
2022-11-27 20:06:55 +0100

    created property dependencies for the height, width and strokethickness

---
+++ Wpf_Rectangle/CustomRectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Rectangle
+{
+    public class CustomRectangle : UIElement
+    {
+
+
+        public double Width
+        {
+            get { return (double)GetValue(WidthProperty); }
+            set { SetValue(WidthProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty WidthProperty =
+            DependencyProperty.Register("Width", typeof(double), typeof(CustomRectangle), new PropertyMetadata(0));
+
+
+
+
+        public double Height
+        {
+            get { return (double)GetValue(HeightProperty); }
+            set { SetValue(HeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeightProperty =
+            DependencyProperty.Register("Height", typeof(double), typeof(CustomRectangle), new PropertyMetadata(0));
+
+
+
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+       
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CustomRectangle), new PropertyMetadata(0));
+
+
+
+
+
+    }
+}
