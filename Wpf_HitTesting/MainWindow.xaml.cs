f29c39dbaa6ee035136ac9bac31bd1978c2bb882
giddy11 <edoghotugiddy@gmail.com>
2022-11-29 13:32:24 +0100

    HitTesting

---
+++ Wpf_HitTesting/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Wpf_HitTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void canvas1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
