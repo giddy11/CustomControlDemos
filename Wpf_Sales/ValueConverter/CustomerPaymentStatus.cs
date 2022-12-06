60bd4e8fcc407408544341b186d7b03d85b463ff
giddy11 <edoghotugiddy@gmail.com>
2022-12-06 13:24:41 +0100

    hhh

---
+++ Wpf_Sales/ValueConverter/CustomerPaymentStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wpf_Sales.ValueConverter
+{
+    public class CustomerPaymentStatus : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (PaymentStatus)value;
+            switch (value)
+            {
+                case PaymentStatus.Paid: return "M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z";
+                case PaymentStatus.Refunded: return "M20 13.5V20H18V13.5C18 11 16 9 13.5 9H7.83L10.91 12.09L9.5 13.5L4 8L9.5 2.5L10.92 3.91L7.83 7H13.5C17.09 7 20 9.91 20 13.5Z";
+                default:
+                    return "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
